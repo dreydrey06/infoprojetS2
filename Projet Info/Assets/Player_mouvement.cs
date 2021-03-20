4a5ea737e5f5c4c37b8c5f4a4ec4d056d3803b22
Audrey <audrey.stfelix@gmail.com>
2021-03-20 18:38:36 +0100

    Début du projet sur Unity
    Pour l'instant il y a un cube qui bouge

---
+++ Projet Info/Assets/Player_mouvement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Player_mouvement : MonoBehaviour {
+
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	rb.AddForce(0, 0, 2000 * Time.deltaTime);
+        
+    }
+}
