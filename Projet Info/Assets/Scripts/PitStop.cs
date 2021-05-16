f55ae2bc3f435c208289aaf9e97da4319f5bed7b
Audrey <audrey.stfelix@gmail.com>
2021-05-16 23:59:03 +0200

    Level 2 + pit stop !
    Fin du niveau deux avec un pit stop : un "mini jeu" dans le jeu. Le jeu s'arrête et il faut cliquer sur 4 pneus le plus vite possible puis la course reprend

---
+++ Projet Info/Assets/Scripts/PitStop.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PitStop : MonoBehaviour
+{
+   
+    public GameObject PitStopUI;
+    public GameObject GameModeUI;
+    public GameObject trigger;
+	private bool finished = false;
+	public GameObject Timer;
+ 
+    
+    void OnTriggerEnter() 
+    {
+    	if (finished == false)
+    	{
+        	Debug.Log("aaah");
+        	Timer.SetActive(true);
+        	Pause();
+        	
+        }
+    }
+   
+    public void Resume()
+    {
+        PitStopUI.SetActive(false);
+        GameModeUI.SetActive(true);
+        trigger.SetActive(false);
+        Time.timeScale = 1f;
+        
+    }
+    public void Pause()
+    {
+        PitStopUI.SetActive(true);
+        GameModeUI.SetActive(false);
+        Time.timeScale = 0f;
+        
+    }
+
+}
+
