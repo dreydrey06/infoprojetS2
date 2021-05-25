34dbdea15bf93b0268798e8bb05d9be2a240005e
Audrey <audrey.stfelix@gmail.com>
2021-05-25 16:31:20 +0200

    ème: Auto stash before checking out "HEAD"

---
+++ Projet Info/Assets/Scripts/LoadLevel2.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevel2 : MonoBehaviour
+{
+    public void LoadTheLevel2()
+    {
+    	SceneManager.LoadScene(2);
+    }
+}
